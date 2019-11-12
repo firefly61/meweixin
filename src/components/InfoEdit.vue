f7f991a5f6b4ad3a58d151d1a9e5af8b07cc32f2
刘颖 <liuying@liuyingdeMacBook-Pro.local>
2019-11-12 18:30:46 +0800

    完善信息

--- src/components/InfoEdit.vue
+++ src/components/InfoEdit.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="infoEdit">
+  <div id="infoEdit">
     <h1 class="title-box">信息完善</h1>
     <cube-form :model="model" @validate="validateHandler" @submit="submitHandler">
       <cube-form-group>
-        <cube-form-item :field="fields[0]"></cube-form-item>
-        <cube-form-item :field="fields[1]"></cube-form-item>
+        <cube-form-item :field="fields[0]">
+        </cube-form-item>
+        <cube-form-item :field="fields[1]">
+        </cube-form-item>
         <cube-form-item :field="fields[2]"></cube-form-item>
         <cube-form-item :field="fields[3]"></cube-form-item>
         <cube-form-item :field="fields[4]">
-          <cube-button class="date-btn" @click="showDatePicker">{{model.dateValue || 'Please select date'}}</cube-button>
-          <date-picker ref="datePicker" title="请选择日期" :min="new Date(1960, 1, 1)" :max="new Date()" :value="new Date()"
-            @select="dateSelectHandler">
+          <cube-button class="date-btn" @click="showDatePicker">{{formatDate || 'Please select date'}}
+          </cube-button>
+          <date-picker ref="datePicker" title="请选择日期" :min="new Date(1960, 1, 1)" :max="new Date()"
+            :value="new Date(model.dateValue)" @select="dateSelectHandler">
           </date-picker>
         </cube-form-item>
       </cube-form-group>
       <cube-form-group>
-        <cube-button type="submit">Submit</cube-button>
+        <cube-button class="submit-btn" type="submit">Submit</cube-button>
       </cube-form-group>
     </cube-form>
   </div>
@@ -32,8 +35,9 @@
         model: {
           name: '刘颖',
           phone: '13611111123',
-          sexy: 2,
-          dateValue: this.dateFormat(new Date())
+          sexy: 1,
+          dateValue: new Date(1993, 9, 13),
+          card: ''
         },
         fields: [{
             type: 'input',
@@ -42,6 +46,7 @@
             props: {
               placeholder: '请输入姓名'
             },
+            valid: undefined,
             rules: {
               required: true
             }
@@ -54,7 +59,11 @@
               placeholder: '请输入电话号'
             },
             rules: {
-              required: true
+              required: true,
+              len: 11
+            },
+            messages: {
+              len: '请输入正确的号码格式'
             }
           },
           {
@@ -65,11 +74,19 @@
               placeholder: '请输入证件号'
             },
             rules: {
-              required: true
+              required: true,
+              len: 18
             },
             events: {
-              blur(e) {
-                console.log(e.target.value)
+              blur: (e) => {
+                let carded = e.target.value;
+                this.model.sexy = carded && carded.charAt(16) % 2 ? 1 : 2;
+                console.log(this.model.sexy)
+                let year = carded.slice(6, 10);
+                let month = Number(carded.slice(10, 12)) -1;
+                let day = carded.slice(12, 14);
+                this.model.dateValue = new Date(year, month, day);
+                console.log(year, month, day, this.model.dateValue)
               }
             }
           },
@@ -100,9 +117,28 @@
         ]
       }
     },
+    computed: {
+      formatDate: function() {
+        let year = this.model.dateValue.getFullYear(),
+          month = this.model.dateValue.getMonth() + 1,
+          day = this.model.dateValue.getDate();
+        return year + '-' + month + '-' + day;
+      }
+    },
+    mounted() {
+      // this.model.phone = this.hideMobile();
+    },
     methods: {
-      submitHandler() {
+      submitHandler(e) {
+        e.preventDefault()
+        console.log('submit', e)
         console.log(this.model)
+        // let temp = JSO
+        this.$createDialog({
+        type: 'warn',
+        content: `Selected Item: <br/> - date: <span style="display: block; width: 100%; word-break:break-all;">${JSON.stringify(this.model)}</span> <br/> }`,
+        icon: 'cubeic-alert'
+      }).show()
       },
       validateHandler(result) {
         this.validity = result.validity
@@ -114,13 +150,10 @@
       },
       dateSelectHandler(date, selectedVal, selectedText) {
         console.log(selectedVal, selectedText);
-        this.model.dateValue = this.dateFormat(date);
+        this.model.dateValue = date;
       },
-      dateFormat(date) {
-        let year = date.getFullYear(),
-        month = date.getMonth() + 1,
-        day = date.getDate();
-        return year + '-' + month + '-' + day;
+      hideMobile() {
+        return this.model.phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2');
       }
     },
     components: {
@@ -131,14 +164,34 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.title-box {
-  padding: 1rem;
-  font-size: 1.25rem;
-}
-.date-btn {
-  background: transparent;
-  text-align: left;
-  color: inherit;
-  padding-left: 0;
-}
+  .title-box {
+    padding: 1rem;
+    font-size: 1.25rem;
+  }
+
+  .cube-validator-msg {
+    border: solid 1px red;
+    font-size: .75rem;
+    display: block
+  }
+
+  .date-btn {
+    background: transparent;
+    text-align: left;
+    color: inherit;
+    padding-left: 0;
+  }
+
+  .submit-btn {
+    background: rgb(40, 192, 40);
+    width: 85%;
+    margin: 0 auto;
+    margin-top: 1rem;
+    padding: 1rem;
+  }
+
+  .cube-dialog-content-def > p {
+    width: 80%;
+    word-break: break-all;
+  }
 </style>
